338c564d7feffffde32595ba36012eec73fdcebd
Mike Mestebeld <mike.mestebeld@sherpaan.nl>
2024-04-22 18:26:34 +0200

    added error handling to DiscordHandler

--- src/services/discordService.ts
+++ src/services/discordService.ts
@@ -10,30 +10,28 @@ export const DiscordInteractionHandler = async (
   try {
     const { type, id, data } = req.body;
 
+    console.log("Received interaction", req.body);
+
     if (type === InteractionType.PING) {
+      console.log("Responding to ping");
       return res.status(200).json({ type: InteractionResponseType.PONG });
     }
 
     if (type === InteractionType.APPLICATION_COMMAND) {
       const { name } = data;
 
-      // "test" command
       if (name === "test") {
-        // Send a message into the channel where command was triggered from
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            // Fetches a random emoji to send from a helper function
             content: serverStatus
               ? `Server is ${serverStatus.status}`
-              : "Server status is currenty unknown",
+              : "Server status is currently unknown",
           },
         });
       }
-
-      // If the command is not recognized, return a generic error message
-      return res.status(400).json({ message: "Unknown command" });
     }
+    return res.status(400).json({ message: "Unknown command" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error", error });
